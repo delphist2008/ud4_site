176d10f7d84ba9929f1fe64e4d8a5b62641b431c
delphist2008 <delphist2008@gmail.com>
2015-12-11 00:56:00 +0600

    slide transition, todos

--- slider/ud4_slider/js/main.js
+++ slider/ud4_slider/js/main.js
@@ -1,7 +1,9 @@
 (function($) {
 
-// Set variables
-
+//FIXME: prevent slide changing during animation
+//FIXME: navigation buttons size&transparancy  changes
+//TODO: proper layout for slide
+//TODO: mousemove
 
 function getRandomInt(min, max)
 {
@@ -17,19 +19,18 @@ var $activeSlide = $(".active"),
     $slideNavNextA = $(".slideNavNext a"),
     $hero = $(".hero");
 
-	/*	// Mouse move tilt effect
-	$(document).mousemove(function(event){
+		// Mouse move tilt effect
+	$(".slideImg").mousemove(function(event){
 	  
 		// Detect mouse position
-		var xPos = (event.clientX/$(window).width())-0.5;
-		var yPos = (event.clientY/$(window).height())-0.5;
+		var xPos = ((event.clientX/$(window).width())-0.5)*(-90);
+		var yPos = ((event.clientY/$(window).height())-0.5)*(-90);
 
 		// Tilt the hero container
-		TweenLite.to($hero, 0.6, { rotationY:5*xPos, rotationX:5*yPos, ease:Power1.easeOut, transformPerspective:900, transformOrigin:"center" });
+		TweenLite.to($(".slideImg"), 0.6, { backgroundPosition:"xPos  yPos", ease:Power1.easeOut});
 
-		// Update text on the page with the current mouse position
-		$(".bottom strong").text(event.pageX + ", " + event.pageY);
-	});*/
+
+	});
 	
 
 	// Init function that run on page load
@@ -39,7 +40,7 @@ var $activeSlide = $(".active"),
 	  TweenLite.set($homeSlide.not($activeSlide), {autoAlpha: 0});
 
 	  // Disable arrow down on page load
-	  TweenLite.set($slideNavPrev, {autoAlpha: 0.2});
+	  //TweenLite.set($slideNavPrev, {autoAlpha: 0.2});
 	  
 	}
 
@@ -48,7 +49,11 @@ var $activeSlide = $(".active"),
 
 
 	function goToNextSlide(slideOut, slideIn){
-	  var tl = new TimelineLite(),
+		
+		var tl1 = new TimelineMax({repeat:1, yoyo:true});
+		tl1.append(new TweenLite.to($(".homeSlide"), 0.5, {x: '+=30px', ease:Power1.easeInOut}, 0));
+		
+		var tl = new TimelineMax(),
 	    size = $('.homeSlide').length;
 		if(slideIn.length !== 0){
 			tl
@@ -57,7 +62,7 @@ var $activeSlide = $(".active"),
 				.to(slideIn, 1, {y: '-=100%', ease:Power2.easeInOut}, 0)
 				.to(slideOut, 1, {y: '-=100%', ease:Power2.easeInOut}, 0);
 		} 
-		TweenLite.set($slideNavPrev, {autoAlpha: 1});
+		//TweenLite.set($slideNavPrev, {autoAlpha: 1});
 		//if(index === size){
 		//  TweenLite.to($slideNavNext, 0.3, {autoAlpha: 0.2, ease:Linear.easeNone});
 		//}
@@ -79,24 +84,25 @@ var $activeSlide = $(".active"),
 	// Go to previous slide - pass 2 parameters - slideOut and slideIn
 	function goToPreviousSlide(slideOut, slideIn){
 	  
+	  var tl1 = new TimelineMax({repeat:1, yoyo:true});
+		tl1.append(new TweenLite.to($(".homeSlide"), 0.5, {x: '-=30px', ease:Power1.easeInOut}, 0));
+		
 	  var tl = new TimelineLite(),
-	    index = slideIn.index(),
 	    size = $('.top .homeSlide').length;
 	  
 	  if(slideIn.length !== 0){
 	    tl
 			.set(slideIn, {y:"-100%", autoAlpha: 1,className: '+=active'})
 			.set(slideOut, {className: '-=active'})
-			.to(slideIn, 2, {y: '+=100%', ease:Power3.easeInOut}, 0)
-			.to(slideOut, 2, {y: '+=100%', ease:Power3.easeInOut}, 0);
-	  
+			.to(slideIn, 1, {y: '+=100%', ease:Power3.easeInOut}, 0)
+			.to(slideOut, 1, {y: '+=100%', ease:Power3.easeInOut}, 0);
 	  } 
 
-	  TweenLite.set($slideNavNext, {autoAlpha: 1});
+	 // TweenLite.set($slideNavNext, {autoAlpha: 1});
 	  
-	  if(index === 1){
-	    TweenLite.to($slideNavPrev, 0.3, {autoAlpha: 0.2, ease:Linear.easeNone});
-	  }
+	 // if(index === 1){
+	  //  TweenLite.to($slideNavPrev, 0.3, {autoAlpha: 0.2, ease:Linear.easeNone});
+	 // }
 	  
 	}
 	 
@@ -104,10 +110,9 @@ var $activeSlide = $(".active"),
 	$slideNavPrev.click(function (e) {
 	  e.preventDefault();
 	  
-	  var slideOut = $('.homeSlide.active');
-	 
-	    var  slideIn = $('.homeSlide.active').prev('.homeSlide');
-	  
+	  var slideOut = $('.homeSlide.active'),
+	  slideIn = $('.homeSlide:not(.active)');
+		  slideIn =  slideIn [getRandomInt(0, slideIn.length-1)];
 	  goToPreviousSlide(slideOut, slideIn);
 	  
 	});
