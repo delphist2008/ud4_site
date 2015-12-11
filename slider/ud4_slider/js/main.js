e1b759776badb1ab19089fb6cd25d7ab06f8ec09
delphist2008 <delphist2008@gmail.com>
2015-12-11 12:17:10 +0600

    cleanup, prevent from changing slide during playback

--- slider/ud4_slider/js/main.js
+++ slider/ud4_slider/js/main.js
@@ -1,9 +1,12 @@
 (function($) {
 
+
 //FIXME: prevent slide changing during animation
-//FIXME: navigation buttons size&transparancy  changes
+//FIXME: navigation buttons size & transparency  changes
 //TODO: proper layout for slide
 //TODO: mousemove
+//TODO: full info panel
+//TODO: random/not random navigation
 
 function getRandomInt(min, max)
 {
@@ -19,6 +22,9 @@ var $activeSlide = $(".active"),
     $slideNavNextA = $(".slideNavNext a"),
     $hero = $(".hero");
 
+	 animation = {inProgress : false};
+	
+
 		// Mouse move tilt effect
 	$(".slideImg").mousemove(function(event){
 	  
@@ -26,8 +32,7 @@ var $activeSlide = $(".active"),
 		var xPos = ((event.clientX/$(window).width())-0.5)*(-90);
 		var yPos = ((event.clientY/$(window).height())-0.5)*(-90);
 
-		// Tilt the hero container
-		TweenLite.to($(".slideImg"), 0.6, { backgroundPosition:"xPos  yPos", ease:Power1.easeOut});
+		//TweenLite.to($(".slideImg"), 0.6, { backgroundPosition:"xPos  yPos", ease:Power1.easeOut});
 
 
 	});
@@ -49,18 +54,19 @@ var $activeSlide = $(".active"),
 
 
 	function goToNextSlide(slideOut, slideIn){
-		
+		animation.inProgress = true;
 		var tl1 = new TimelineMax({repeat:1, yoyo:true});
 		tl1.append(new TweenLite.to($(".homeSlide"), 0.5, {x: '+=30px', ease:Power1.easeInOut}, 0));
 		
-		var tl = new TimelineMax(),
+		var tl = new TimelineMax({onComplete: function (){animation.inProgress = false;}}),
 	    size = $('.homeSlide').length;
 		if(slideIn.length !== 0){
 			tl
-				.set(slideIn, {y:"100%", autoAlpha: 1,className: '+=active'})
+				.set(slideIn, {y:"110%",className: '+=active'})
 				.set(slideOut, {className: '-=active'})
-				.to(slideIn, 1, {y: '-=100%', ease:Power2.easeInOut}, 0)
-				.to(slideOut, 1, {y: '-=100%', ease:Power2.easeInOut}, 0);
+				.to(slideIn, 1, {y: '-=110%',autoAlpha: 1, ease:Power2.easeInOut}, 0)
+				.to(slideOut, 1, {y: '-=110%',autoAlpha: 0, ease:Power2.easeInOut}, 0);
+				
 		} 
 		//TweenLite.set($slideNavPrev, {autoAlpha: 1});
 		//if(index === size){
@@ -73,29 +79,31 @@ var $activeSlide = $(".active"),
 	$slideNavNext.click(function (e) {
 	  e.preventDefault();
 	  
+	 if (!animation.inProgress)
+	  {
 	  var slideOut = $('.homeSlide.active'),
 		   slideIn = $('.homeSlide:not(.active)');
 		  slideIn =  slideIn [getRandomInt(0, slideIn.length-1)];
 	  
 	  goToNextSlide(slideOut, slideIn);
+	  };
 	  
 	});
 
-	// Go to previous slide - pass 2 parameters - slideOut and slideIn
 	function goToPreviousSlide(slideOut, slideIn){
-	  
+	  animation.inProgress = true;
 	  var tl1 = new TimelineMax({repeat:1, yoyo:true});
 		tl1.append(new TweenLite.to($(".homeSlide"), 0.5, {x: '-=30px', ease:Power1.easeInOut}, 0));
 		
-	  var tl = new TimelineLite(),
+	  var tl = new TimelineLite({onComplete: function (){animation.inProgress = false;}}),
 	    size = $('.top .homeSlide').length;
 	  
 	  if(slideIn.length !== 0){
 	    tl
-			.set(slideIn, {y:"-100%", autoAlpha: 1,className: '+=active'})
-			.set(slideOut, {className: '-=active'})
-			.to(slideIn, 1, {y: '+=100%', ease:Power3.easeInOut}, 0)
-			.to(slideOut, 1, {y: '+=100%', ease:Power3.easeInOut}, 0);
+			.set(slideIn, {y:"-110%",className: '+=active'})
+			.set(slideOut, { className: '-=active'})
+			.to(slideIn, 1, {y: '+=110%', autoAlpha: 1,ease:Power3.easeInOut}, 0)
+			.to(slideOut, 1, {y: '+=110%', autoAlpha: 0, ease:Power3.easeInOut}, 0);
 	  } 
 
 	 // TweenLite.set($slideNavNext, {autoAlpha: 1});
@@ -109,11 +117,13 @@ var $activeSlide = $(".active"),
 	// Navigation click - go to the Prev Slide
 	$slideNavPrev.click(function (e) {
 	  e.preventDefault();
-	  
+	  if (!animation.inProgress)
+	  {
 	  var slideOut = $('.homeSlide.active'),
 	  slideIn = $('.homeSlide:not(.active)');
 		  slideIn =  slideIn [getRandomInt(0, slideIn.length-1)];
 	  goToPreviousSlide(slideOut, slideIn);
+	  };
 	  
 	});
 
