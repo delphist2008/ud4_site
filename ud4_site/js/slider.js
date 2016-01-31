0dd26feae1f37d8d3f2d3c9e3d53dd8d337bca9c
delphist2008 <delphist2008@gmail.com>
2016-01-31 17:49:08 +0600

    cleanup

--- ud4_site/js/slider.js
+++ ud4_site/js/slider.js
@@ -1,5 +1,5 @@
 (function($) {
-//TODO: раз уже есть таймлайны для всех сторон - нужно их и использовать для переходов везде, в том числе по колесу и кнопкам.
+
 function getRandomInt(min, max)
 {
   return Math.floor(Math.random() * (max - min + 1)) + min;
@@ -18,7 +18,7 @@ var $activeSlide = $(".active"),
     $homeSlide = $(".homeSlide"),
 	$bcg = $(".bcg"),
     $hero = $(".hero"),
-	$randomTimeLine = new TimelineMax({ repeat:-1, paused:true, onRepeat: function () { /*getSlide("down", true);*/}});
+	$randomTimeLine = new TimelineMax({ repeat:-1, paused:true, onRepeat: function () { getSlide("down", true);}});
 	$randomTimeLine.to($("nothing"), 8, {width:"100px"});
 	//$logoTimeline = new TimelineMax({paused: false, repeat:0, onComplete: function () { getSlide("down", true);}}); 
 	//$logoTimeline.to($("#nothing"), 4, {width:"100px"});
@@ -34,9 +34,9 @@ var $activeSlide = $(".active"),
 	 $Tright=new TimelineMax({ paused:true, onReverseComplete : function (){tlcb (); }, onComplete: function (){ tlcb ();}});
 	var _Y;
 	var _X;
-	var dir = null;//двигаем по горизонтали или вертикали или не двигаем
-	 sens = 0.1; //насколько надо сдвинуть, чтобы определилась ось
-	prgSens = 0.3; //насколько сильно сдвинуть, чтобы оторвать от края
+	dir = null;//двигаем по горизонтали или вертикали или не двигаем
+	sens = 0.1; //насколько надо сдвинуть, чтобы определилась ось
+	prgSens = 0.15; //насколько сильно сдвинуть, чтобы оторвать от края
 	$("html").mousemove(function(event){
 		if (!animator.inProgress)
 		{
@@ -63,7 +63,6 @@ var $activeSlide = $(".active"),
 	  TweenLite.set($homeSlide.not($activeSlide), {y:"-100%"});
 	   TweenLite.set($(".imageContainer").not($(".active")), {y:"-100%"});
 	
-	 
 	  TweenLite.set($("#up"), {visibility: "visible"});
 	  $homeSlide.toggleClass( "homeSlide homeSlide_anim" );
 	  $bcg.toggleClass( "bcg bcg_anim" );
@@ -74,8 +73,6 @@ var $activeSlide = $(".active"),
 	  });
 	  fixAdaptive();
 	  clearTs() ;
-	
-	  
 	}
 
 	$(window).load(function(){
@@ -84,9 +81,9 @@ var $activeSlide = $(".active"),
 
 	function goToNextSlide(slideOut, slideIn){
 		animator.inProgress = true;
-		// var a = $(slideIn.filter(".homeSlide_anim")).find("a");
-		// $(a).css("margin-bottom","300px");
-		//  TweenLite.to(a,animator.transitionTime*2, {marginBottom:"15", ease:Power4.easeInOut});	
+		 var a = $(slideIn.filter(".homeSlide_anim")).find("a");
+		 $(a).css("margin-bottom","300px");
+		TweenLite.to(a,animator.transitionTime*2, {marginBottom:"15", ease:Power4.easeInOut});	
 		 var ic = slideIn.filter(".imageContainer");
 		 var hsa = slideIn.filter(".homeSlide_anim");
 		var tl = new TimelineMax({ onComplete: function (){animator.inProgress = false;}});
@@ -98,43 +95,23 @@ var $activeSlide = $(".active"),
 				.to(slideOut, animator.transitionTime, {y: '-=100%',z: 0.05,  rotationZ: 0.01,force3D:true, ease:Power1.easeInOut},0);
 	};
 
-	function goToRightSlide(slideOut, slideIn){
-		animator.inProgress = true;
-		
-		var tl = new TimelineMax({onComplete: function (){animator.inProgress = false;}});
-			tl
-				.set(slideIn, {x:"100%", className: '+=active'})
-				.set(slideOut, {className: '-=active'})
-				.to(slideIn.filter(".imageContainer"), animator.transitionTime, {x: '-=100%',opacity: 1, ease:Power2.easeInOut}, 0)
-				.to(slideIn.filter(".homeSlide_anim"), animator.transitionTime*1.35, {x: '-=100%',opacity: 1, ease:Power2.easeInOut}, 0)
-				.to(slideOut, animator.transitionTime, {x: '-=100%',opacity: 0, ease:Power2.easeInOut}, 0);
-	};
-	
 	function goToPreviousSlide(slideOut, slideIn){
 	  animator.inProgress = true;
 		var a = $(slideIn.filter(".homeSlide_anim")).find("a");
 		 $(a).css("margin-bottom","-300px");
 		 TweenLite.to(a,animator.transitionTime*2, {marginBottom:"15", ease:Power4.easeInOut});	
-	  var tl = new TimelineMax({onComplete: function (){animator.inProgress = false;}});
-	    tl
-			.set(slideIn, {y:"-100%",className: '+=active'})
-			.set(slideOut, { className: '-=active'})
-			.to(slideIn.filter(".imageContainer"), animator.transitionTime, {y: '+=100%', opacity: 1,ease:Power1.easeInOut}, 0)
-			.to(slideIn.filter(".homeSlide_anim"), animator.transitionTime*1.35, {y: '+=100%', opacity: 1,ease:Power2.easeInOut}, 0)
-			.to(slideOut, animator.transitionTime, {y: '+=100%', opacity: 0, ease:Power2.easeInOut}, 0);
+		  var ic = slideIn.filter(".imageContainer");
+		 var hsa = slideIn.filter(".homeSlide_anim");
+		 var tl = new TimelineMax({ onComplete: function (){animator.inProgress = false;}});
+			tl
+				.set(slideIn, {y:"-100%", className: '+=active'})
+				.set(slideOut, {className: '-=active'})
+				.to(ic, animator.transitionTime, {y: '+=100%', z: 0.1,  rotationZ: 0.01,force3D:true, ease:Power1.easeInOut},0)
+				.to(hsa, animator.transitionTime*1.35, {y: '+=100%',z: 0.1,  rotationZ: 0.01,force3D:true, ease:Power1.easeInOut},0)
+				.to(slideOut, animator.transitionTime, {y: '+=100%',z: 0.05,  rotationZ: 0.01,force3D:true, ease:Power1.easeInOut},0);
 	};
 	
-	function goToLeftSlide(slideOut, slideIn){
-	  animator.inProgress = true;
-		
-	  var tl = new TimelineMax({onComplete: function (){animator.inProgress = false;}});
-	    tl
-			.set(slideIn, {x:"-100%",className: '+=active'})
-			.set(slideOut, { className: '-=active'})
-			.to(slideIn.filter(".imageContainer"), animator.transitionTime, {x: '+=100%', opacity: 1,ease:Power2.easeInOut}, 0)
-			.to(slideIn.filter(".homeSlide_anim"), animator.transitionTime*1.35, {x: '+=100%', opacity: 1,ease:Power2.easeInOut}, 0)
-			.to(slideOut, animator.transitionTime, {x: '+=100%', opacity: 0, ease:Power2.easeInOut}, 0);
-	};
+
 	 	
 	function getSlide(direction, random)
 	{
@@ -142,7 +119,7 @@ var $activeSlide = $(".active"),
 	TweenLite.to($(".whiteBar"), animator.transitionTime, {visibility:"visible", opacity: 1, delay: animator.transitionTime});
 	//$logoTimeline.stop();
 	
-	//$randomTimeLine.play();
+	$randomTimeLine.play();
 	if (!animator.inProgress)
 	  {
 		TweenLite.set($(".homeSlide_anim"),  { transform:"none"});
@@ -156,17 +133,10 @@ var $activeSlide = $(".active"),
 		if (random)
 			{
 				si = slideOut.siblings(".homeSlide_anim");
-				si = si[getRandomInt(1, si.length-1)];
+				si = si[getRandomInt(0, si.length-1)];
 				img = $(".imageContainer[data-case="+si.attributes["data-case"].value+"]");
 				si = $(si).add(img);
-				if (direction == "down")
-				goToNextSlide(slideOut.add(soim), si);
-				if (direction == "right")
-				goToRightSlide(slideOut.add(soim), si);
-				if (direction == "up")
-				goToPreviousSlide(slideOut.add(soim), si);
-				if (direction == "left")
-				goToLeftSlide(slideOut.add(soim), si);
+				goToNextSlide(slideOut.add(soim), si);	
 		}
 		else	
 		{
@@ -189,7 +159,7 @@ var $activeSlide = $(".active"),
 			
 			if (direction == "up" || direction == "left")
 			{
-				if (slideIn.index(slideOut)  >  1)
+				if (slideIn.index(slideOut)  >  0)
 				{
 					si = slideOut.prevAll(".homeSlide_anim");
 					si = si[0];
@@ -205,16 +175,16 @@ var $activeSlide = $(".active"),
 			};
 		  }
 	  };
-	  
+	  clearTs();
 	};
 	
-	/*$( "html" ).mousewheel(function(event){
+	$( "html" ).mousewheel(function(event){
 		$randomTimeLine.restart();
 		if (event.deltaY < 0)
 			getSlide("up", false)
 		else
 			getSlide("down", false);
-	});*/
+	});
 	
 	
 	
@@ -224,11 +194,9 @@ var $activeSlide = $(".active"),
 		var slideIn = $('.homeSlide_anim');
 		var si;
 		var img;
-		
+		var a = $(slideIn.filter(".homeSlide_anim")).find("a");
 		if (direction == "down" || direction == "right" )
 			{
-				
-				
 				if (slideIn.index(slideOut)  >  0)
 				{
 					si = slideOut.prevAll(".homeSlide_anim");
@@ -243,25 +211,21 @@ var $activeSlide = $(".active"),
 				
 				if (direction == "down")
 				{
-				
-				
-				
+					
+					
 				$Tdown
 					.set($(".imageContainer"),  { transform:"none"})
 					.set($(".homeSlide_anim"),  { transform:"none"})
 					.set(si, {y:"-100%", className: '+=active'})
 					.set(slideOut.add(soim), {className: '-=active'})
+					.set(a, {marginBottom:"300px"})
 					.to(ic, animator.transitionTime, {y: '+=100%', z: 0.1,  rotationZ: 0.01,force3D:true,  ease:Sine.easeIn},0)
 					.to(hsa, animator.transitionTime*1.35, {y: '+=100%',z: 0.1,  rotationZ: 0.01,force3D:true, ease:Sine.easeIn},0)
-					.to(slideOut.add(soim), animator.transitionTime, {y: '+=100%',z: 0.05,  rotationZ: 0.01,force3D:true, ease:Sine.easeIn},0);
-					
-					
-				
+					.to(slideOut.add(soim), animator.transitionTime, {y: '+=100%',z: 0.05,  rotationZ: 0.01,force3D:true, ease:Sine.easeIn},0)
+					.to(a,animator.transitionTime*2, {marginBottom:"15px", ease:Power4.easeInOut},0);
 				}
 				else
 				{
-					
-				
 				$Tright
 					.set($(".imageContainer"),  { transform:"none"})
 					.set($(".homeSlide_anim"),  { transform:"none"})
@@ -270,15 +234,12 @@ var $activeSlide = $(".active"),
 					.to(ic, animator.transitionTime, {x: '+=100%', z: 0.1,  rotationZ: 0.01,force3D:true,  ease:Sine.easeIn},0)
 					.to(hsa, animator.transitionTime*1.35, {x: '+=100%',z: 0.1,  rotationZ: 0.01,force3D:true, ease:Sine.easeIn},0)
 					.to(slideOut.add(soim), animator.transitionTime, {x: '+=100%',z: 0.05,  rotationZ: 0.01,force3D:true, ease:Sine.easeIn},0);
-					
-					
 				}
 				
 			};
 			
 			if (direction == "up" || direction == "left")
 			{
-				
 				if (slideIn.index(slideOut)  <  slideIn.length-1)
 				{
 					si = slideOut.nextAll(".homeSlide_anim");
@@ -293,24 +254,19 @@ var $activeSlide = $(".active"),
 				
 				if (direction == "up")
 				{
-				
-					
 				$Tup
 					.set($(".imageContainer"),  { transform:"none"})
 					.set($(".homeSlide_anim"),  { transform:"none"})
 					.set(si, {y:"100%", className: '+=active'})
 					.set(slideOut.add(soim), {className: '-=active'})
+					.set(a, {marginBottom:"300px"})
 					.to(ic, animator.transitionTime, {y: '-=100%', z: 0.1,  rotationZ: 0.01,force3D:true,  ease:Sine.easeIn},0)
 					.to(hsa, animator.transitionTime*1.35, {y: '-=100%',z: 0.1,  rotationZ: 0.01,force3D:true, ease:Sine.easeIn},0)
+					.to(a,animator.transitionTime*2, {marginBottom:"15px", ease:Power4.easeInOut},0)
 					.to(slideOut.add(soim), animator.transitionTime, {y: '-=100%',z: 0.05,  rotationZ: 0.01,force3D:true, ease:Sine.easeIn},0);
-					
-					
-				
 				}
 				else
 				{
-				
-				
 				$Tleft
 					.set($(".imageContainer"),  { transform:"none"})
 					.set($(".homeSlide_anim"),  { transform:"none"})
@@ -319,12 +275,8 @@ var $activeSlide = $(".active"),
 					.to(ic, animator.transitionTime, {x: '-=100%', z: 0.1,  rotationZ: 0.01,force3D:true,  ease:Sine.easeIn},0)
 					.to(hsa, animator.transitionTime*1.35, {x: '-=100%',z: 0.1,  rotationZ: 0.01,force3D:true, ease:Sine.easeIn},0)
 					.to(slideOut.add(soim), animator.transitionTime, {x: '-=100%',z: 0.05,  rotationZ: 0.01,force3D:true, ease:Sine.easeIn},0);
-					
-					
 				}
-				
 			};
-			
 	};
 	
 	$( "html" ).on('touchstart', function(e){
@@ -332,7 +284,7 @@ var $activeSlide = $(".active"),
 		{
 		_Y = e.originalEvent.touches[0].pageY;
 		_X = e.originalEvent.touches[0].pageX;
-		
+		$randomTimeLine.restart();
 		dir = null;
 		e.preventDefault();
 		animator.swiping = true;
@@ -340,7 +292,7 @@ var $activeSlide = $(".active"),
 	});
 	
 	$( "html" ).on('touchmove', function(e){
-		
+		$randomTimeLine.restart();
 		e.preventDefault();
 			var dy = (e.originalEvent.touches[0].pageY - _Y)/$("html").height();
 			var dx = (e.originalEvent.touches[0].pageX - _X)/$("html").width();
@@ -361,7 +313,6 @@ var $activeSlide = $(".active"),
 				{
 					$Tright.progress(0);
 					$Tleft.progress(Math.abs(dx));
-					
 				}
 			}
 			else
@@ -371,7 +322,6 @@ var $activeSlide = $(".active"),
 					{
 						$Tup.progress(0);
 						$Tdown.progress(Math.abs(dy));
-						
 					}
 					else
 					{
@@ -380,14 +330,11 @@ var $activeSlide = $(".active"),
 						
 					};
 				}
-			
 	});
-	
-	
-	
 	
 	$( "html" ).on('touchend', function(e){
 		animator.swiping = false;
+		$randomTimeLine.play();
 			if (dir=='horiz')
 			{
 				if ($Tleft.progress()>$Tright.progress() )
@@ -433,7 +380,6 @@ var $activeSlide = $(".active"),
 				}
 			}
 			dir = null;
-			
 	});
 	
 	
@@ -457,6 +403,7 @@ var $activeSlide = $(".active"),
 		$randomTimeLine.restart();
 		getSlide("down", false)	
 	});
+	
 	$( "#menuCloseArea" ).click(function(e){
 		$("#menu_checkbox:checked").prop('checked', false).trigger("change");;
 	});
